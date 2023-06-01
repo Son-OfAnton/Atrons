77152c2d773abce9bd3d5d2762860eb15fd786fa
Son-OfAnton <admasterefe00@gmail.com>
2023-06-02 02:31:27 +0300

    added footer and genre drop down

--- frontend/script/home.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const sidebar = document.querySelector(".sidebar");
-
-hamburger.addEventListener("click", function () {
-  sidebar.style.left = sidebar.style.left === "-300px" ? "0" : "-300px";
-});
